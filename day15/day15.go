042c9efd57caf7ae8e5c892342bdf6dd1c8e5c8c
Viginox <31762120+Viginox@users.noreply.github.com>
2022-12-16 00:24:41 +0100

    Update day 15

--- day15/day15.go
+++ day15/day15.go
@@ -111,9 +111,9 @@ func (g *Grid) InsertAtFront(xAxis int, yAxis int) {
 			}
 		}
 		if g.XOffset+xAxis > g.XReduction {
-			g.XOffset = -(g.XReduction - xAxis)
-		}
-		if g.XReduction >= xAxis {
+			g.XOffset += -(g.XReduction - xAxis)
+            g.XReduction = 0
+		} else if xAxis <= g.XReduction {
 			g.XReduction -= xAxis
 		}
 	}
@@ -134,9 +134,9 @@ func (g *Grid) InsertAtFront(xAxis int, yAxis int) {
 		}
 		// adapt offset
 		if g.YOffset+yAxis > g.YReduction {
-			g.YOffset = -(g.XReduction - yAxis)
-		}
-		if g.YReduction >= yAxis {
+			g.YOffset += -(g.YReduction - yAxis)
+            g.YReduction = 0
+		} else if yAxis <= g.YReduction {
 			g.YReduction -= yAxis
 		}
 	}
@@ -149,14 +149,14 @@ func (g *Grid) GrowToFit(sensor Sensor) {
 	// Reduction to remove too high values
 	//
 	// Check the X position
-	fmt.Println(g)
-	if sensor.Position.X < g.XReduction {
+    /*
+	if g.XReduction == math.MaxInt {
 		g.XReduction = sensor.Position.X
 	}
-	if sensor.Position.Y < g.YReduction {
+	if g.YReduction == math.MaxInt {
 		g.YReduction = sensor.Position.Y
 	}
-	fmt.Println(g)
+    */
 	sensor_x := sensor.Position.X + g.XOffset - g.XReduction
 	max_x := sensor_x + sensor.Distance
 	if max_x >= g.GetWidth() {
@@ -166,6 +166,8 @@ func (g *Grid) GrowToFit(sensor Sensor) {
 	if min_x < 0 {
 		g.InsertAtFront(-(min_x), 0)
 	}
+    // ignore
+    return
 	sensor_y := sensor.Position.Y + g.YOffset - g.YReduction
 	if sensor_y+sensor.Distance >= g.GetHeight() {
 		g.ExtendAtEnd(0, (sensor_y+sensor.Distance)-g.GetHeight()+1)
@@ -175,37 +177,61 @@ func (g *Grid) GrowToFit(sensor Sensor) {
 	}
 }
 
-func (g *Grid) DrawSensor(sensor Sensor) {
-	g.GrowToFit(sensor)
+func (g *Grid) DrawSensor(sensor Sensor, search_row int) {
+    //fmt.Printf("Drawing sensor %v\n", sensor)
+	//g.GrowToFit(sensor)
 	// mark sensor position
-	g.getTileAt(sensor.Position.X, sensor.Position.Y).value = 'S'
+    if sensor.Position.Y == search_row {
+        g.getTileAt(sensor.Position.X, 0).value = 'S'
+    }
 	// mark beacon position
-	g.getTileAt(sensor.ClosestBeacon.X, sensor.ClosestBeacon.Y).value = 'B'
+    if sensor.ClosestBeacon.Y == search_row {
+	    g.getTileAt(sensor.ClosestBeacon.X, 0).value = 'B'
+    }
 	// draw detection range
-	for y := sensor.Position.Y - sensor.Distance; y <= sensor.Position.Y+sensor.Distance; y++ {
+	//for y := sensor.Position.Y - sensor.Distance; y <= sensor.Position.Y+sensor.Distance; y++ {
+    y := search_row
 		dist := sensor.Position.Y - y
 		if dist < 0 {
 			dist *= -1
 		}
 		for x := sensor.Position.X - (sensor.Distance - dist); x <= sensor.Position.X+(sensor.Distance-dist); x++ {
-			tile := g.getTileAt(x, y)
-			if tile.value == 0 {
-				tile.value = '#'
-			}
+            if y == search_row {
+			    tile := g.getTileAt(x, 0)
+			    if tile.value == 0 {
+			    	tile.value = '#'
+			    }
+            }   
 		}
-	}
+	//}
 }
 
 func (g *Grid) CountBeaconNotPresentForRow(row int) (count int) {
-	line := g.Tiles[row+g.YOffset-g.YReduction]
+	//line := g.Tiles[row+g.YOffset-g.YReduction]
+    line := g.Tiles[0]
 	count = 0
-	for _, tile := range line {
+    for _, tile := range line {
 		if tile.value == '#' {
 			count++
 		}
 	}
 	return count
 }
+
+func (g *Grid) GetPossibleBeaconPosition(search_start int, search_end int) (pos Position) {
+    for y := 0; y < g.GetHeight(); y++ {
+        for x := search_start; x < search_end; x++ {
+            tile := g.Tiles[y][x]
+            if tile.value == 0 {
+                return Position{
+                    X: x-g.XOffset,
+                    Y: y,
+                }
+            }
+        }
+    }
+    return Position{X: -1, Y: -1}
+} 
 
 func parseSensor(line string) (sensor Sensor) {
 	parts := strings.Split(line, ":")
@@ -239,22 +265,22 @@ func parseSensor(line string) (sensor Sensor) {
 }
 
 func getGridSize(sensors []Sensor) (oLimits []int) {
-	checkPos := func(pos Position, limits []int) {
-		if pos.X < limits[0] {
-			limits[0] = pos.X
-		} else if pos.X > limits[1] {
-			limits[1] = pos.X
+	checkPos := func(pos Position, dist int, limits []int) {
+		if (pos.X-dist) < limits[0] {
+			limits[0] = pos.X-dist
+		} else if (pos.X+dist) > limits[1] {
+			limits[1] = pos.X+dist
 		}
-		if pos.Y < limits[2] {
-			limits[2] = pos.Y
-		} else if pos.Y > limits[3] {
-			limits[3] = pos.Y
+		if (pos.Y-dist) < limits[2] {
+			limits[2] = pos.Y-dist
+		} else if (pos.Y+dist) > limits[3] {
+			limits[3] = pos.Y+dist
 		}
 	}
 	var limits = []int{0, 0, 0, 0}
 	for _, sensor := range sensors {
-		checkPos(sensor.Position, limits)
-		checkPos(sensor.ClosestBeacon, limits)
+		checkPos(sensor.Position, sensor.Distance, limits)
+		checkPos(sensor.ClosestBeacon, 0, limits)
 	}
 	return limits
 }
@@ -288,27 +314,73 @@ func readFile(filename string) (lines []string) {
 func solve1(grid Grid, limits []int, sensors []Sensor, search_row int) {
 	// skip limits and create a default array. it will grow with demand
 	//grid.Init(limits)
-	grid.Init(1, 1)
+	grid.Init((-limits[0])+limits[1]+1, 1)
+    grid.XOffset = -limits[0]
+    grid.XReduction = 0
+    grid.YReduction = 0
 	for _, sensor := range sensors {
 		if sensor.Position.Y-sensor.Distance <= search_row && search_row <= sensor.Position.Y+sensor.Distance {
-			fmt.Println(sensor)
-			grid.DrawSensor(sensor)
-			grid.Print()
+			grid.DrawSensor(sensor, search_row)
 			//break
 		}
 	}
-	grid.Print()
+	//grid.Print()
 	row := search_row
 	count := grid.CountBeaconNotPresentForRow(row)
 	fmt.Printf("Counted %d positions in line %d where no beacon can be\n", count, row)
+    fmt.Printf("[Solve1] Result: %d\n", count)
+}
+
+func checkBeacon(sensors []Sensor, search_start int, search_end int) Position {
+    for search_row := search_start; search_row < search_end; search_row++ {
+        //fmt.Printf("Searching in row %d\n", search_row)
+        for search_point := search_start; search_point < search_end; search_point++ {
+            //fmt.Printf("Search Point: %d\n", search_point)
+            inSensorRange := false
+            for _, sensor := range(sensors) {
+                if !(sensor.Position.Y - sensor.Distance <= search_row && search_row <= sensor.Position.Y + sensor.Distance) {
+                    continue
+                }
+                //fmt.Printf("Checking sensor %v\n", sensor)
+                dist := sensor.Position.Y - search_row
+                if dist < 0 {
+                    dist *= -1
+                }
+                start_x := sensor.Position.X - (sensor.Distance - dist)
+                end_x := sensor.Position.X + (sensor.Distance - dist)
+                if start_x <= search_point && search_point < end_x {
+                    search_point = end_x
+                    inSensorRange = true
+                    break
+                }
+            }
+            if inSensorRange {
+                continue
+            }
+            if search_point < search_end {
+                return Position{X: search_point, Y: search_row}
+            }
+        }
+    }
+    return Position{X: -1, Y: -1}
+}
+
+func solve2(limits []int, sensors []Sensor, search_start int, search_end int) {
+    fmt.Printf("Searching from %d to %d\n", search_start, search_end)
+    var beaconPos Position
+    beaconPos = checkBeacon(sensors, search_start, search_end) 
+    fmt.Printf("Found possible beacon pos at %v\n", beaconPos)
+    result := beaconPos.X * 4000000 + beaconPos.Y
+    fmt.Printf("[Solve2] Result: %d\n", result)
 }
 
 func main() {
-	input_lines := readFile("test_input.txt")
+	input_lines := readFile("input.txt")
 	sensors := parseInput(input_lines)
 	limits := getGridSize(sensors)
 	fmt.Printf("Limits are: %v\n", limits)
 	grid := Grid{}
 
-	solve1(grid, limits, sensors, 10)
+	solve1(grid, limits, sensors, 2000000)
+    solve2(limits, sensors, 0, 4000000)
 }
